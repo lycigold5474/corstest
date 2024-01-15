0c9efa2fa0944388d82f73488ff6ad0345771c05
lycigold5474 <108188352+lycigold5474@users.noreply.github.com>
2024-01-15 16:53:03 +0900

    cors 글로벌 설정

--- src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
+++ src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -2,6 +2,9 @@ package com.example.restservicecors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestServiceCorsApplication {
@@ -10,4 +13,17 @@ public class RestServiceCorsApplication {
 		SpringApplication.run(RestServiceCorsApplication.class, args);
 	}
 
+	/**
+	 * 글로벌하게 설정할때 사용 컨트롤러 메소드에만 설정 할 수도 있음 (컴트롤러 참조)
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9001");
+			}
+		};
+	}
 }
